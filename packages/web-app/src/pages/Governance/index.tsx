8991ed22569921045c11d23c78b82a81ea0c6722
jmaier6 <jasonmaier06@gmal.com>
2021-12-20 17:22:38 -0800

    changes to governance page

--- packages/web-app/src/pages/Governance/index.tsx
+++ packages/web-app/src/pages/Governance/index.tsx
@@ -18,17 +18,28 @@ const GovernancePage = () => {
             marginBottom: 32,
             fontSize: 20,
             fontWeight: 400,
-            maxWidth: 650,
+            maxWidth: 1000,
             lineHeight: 1.5,
           }}
         >
-          A community-governed treasury for collecting and allocating Loot royalties. 
-          One vote per Loot bag. Currently collecting from <a href="https://loot.exchange" target="_blank">Loot.Exchange</a>
+          A community-governed treasury for the Loot ecosystem. One vote per
+          Loot bag. Funded by a 5% royalty on Loot (for Adventurers) at{" "}
+          <a href="https://loot.exchange" target="_blank">
+            Loot.Exchange
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://https://opensea.io/collection/lootproject"
+            target="_blank"
+          >
+            Opensea
+          </a>
+          .
         </h3>
         <Proposals proposals={proposals} />
         <p>
-          A minimum threshold of 0.1% of the total Loot supply (8 bags) is required to
-          submit proposals
+          A minimum threshold of 0.1% of the total Loot supply (8 bags) is
+          required to submit proposals
         </p>
       </Col>
     </Section>
