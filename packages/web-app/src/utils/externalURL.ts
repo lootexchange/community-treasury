da95bff038ea6f890bf1ad8414a249258850088e
jmaier6 <jasonmaier06@gmal.com>
2021-12-20 17:22:54 -0800

    updated to Loot External URLs

--- packages/web-app/src/utils/externalURL.ts
+++ packages/web-app/src/utils/externalURL.ts
@@ -7,9 +7,9 @@ export enum ExternalURL {
 export const externalURL = (externalURL: ExternalURL) => {
   switch (externalURL) {
     case ExternalURL.discord:
-      return "https://discord.gg/HNq5bE3x5F";
+      return "https://discord.gg/baXP2YPM";
     case ExternalURL.twitter:
-      return "https://twitter.com/LootExchange";
+      return "https://twitter.com/lootproject";
     case ExternalURL.github:
       return "https://github.com/lootexchange/community-treasury";
   }
